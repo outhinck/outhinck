475df357b3cafd313ac81b0109cf464ddbaeeb4e
Outhinck <hello@outhinck.agency>
2024-12-16 11:56:37 +0000

    updates: added templates

--- outhinck/settings.py
+++ outhinck/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v#*hy(a+==ljp1^8m6u26f75_)(^4$%9vrj$5aw9)0-tlwq6%f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh",'www.outhinck.agency','outhinck.agency']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'productionFiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
