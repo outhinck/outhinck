ac028e749306a9849c69c91127639a04490658fa
Outhinck <hello@outhinck.agency>
2024-12-16 14:26:31 +0000

    added contact form

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255,null=True,blank=True)
+    email = models.CharField(max_length=255,null=True,blank=True)
+    phone = models.CharField(max_length=255,null=True,blank=True)
+    subject = models.CharField(max_length=255,null=True,blank=True)
+    message = models.TextField(null=True,blank=True)
